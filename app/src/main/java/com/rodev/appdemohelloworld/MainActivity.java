91938288a31d0e0b1cfd1ec8fcf4ab9aa5d03455
roman <roman.gajardo@gmail.com>
2020-09-28 14:06:03 -0300

    ui inicial

---
+++ app/src/main/java/com/rodev/appdemohelloworld/MainActivity.java
@@ -0,0 +1,14 @@
+package com.rodev.appdemohelloworld;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
