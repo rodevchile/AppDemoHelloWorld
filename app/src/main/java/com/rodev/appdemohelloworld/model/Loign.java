5f49e0245e2b9caae67ba874bd789a6bdb57dfe0
roman <roman.gajardo@gmail.com>
2020-09-30 16:39:39 -0300

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	.idea/vcs.xml
    comment ok

---
+++ app/src/main/java/com/rodev/appdemohelloworld/model/Loign.java
@@ -0,0 +1,10 @@
+package com.rodev.appdemohelloworld.model;
+
+public class Loign {
+    public String username;
+    public String password;
+
+    public Loign(String password) {
+        this.password = password;
+    }
+}
